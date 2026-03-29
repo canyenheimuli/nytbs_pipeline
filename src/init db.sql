fca3e73ff89469db68fdd2189eaac7e5558e5180
Canyen Heimuli <46830067+canyenheimuli@users.noreply.github.com>
2026-03-28 20:58:25 -0700

    Update init db.sql

--- src/init db.sql
+++ src/init db.sql
@@ -1,46 +1,10 @@
 -- ** DB Setup Script (creates the initial tables and metadata) **
 -- * Table Creation *
--- Weekly Lists Table
-IF OBJECT_ID(N'dbo.weekly_lists', N'U') IS NULL
-  BEGIN
-    CREATE TABLE dbo.weekly_lists (
-      list_id SMALLINT NOT NULL,
-      book_rank SMALLINT NOT NULL,
-      isbn13 VARCHAR(20) NOT NULL,	
-      rank_last_period SMALLINT NOT NULL,
-      periods_on_list SMALLINT NOT NULL, 
-      pub_date DATE NOT NULL,
-      pub_date_year SMALLINT NULL,
-      pub_date_month SMALLINT NULL,
-      retrieval_date DATE NOT NULL,
-      PRIMARY KEY (list_id, book_rank, pub_date)
-    );
-  END
-GO
-
--- Monthly Lists Table (PRODUCTION)
-IF OBJECT_ID(N'dbo.monthly_lists', N'U') IS NULL
-  BEGIN
-    CREATE TABLE dbo.monthly_lists (
-      list_id SMALLINT NOT NULL,
-      book_rank SMALLINT NOT NULL,
-      isbn13 VARCHAR(20) NOT NULL,	
-      rank_last_period SMALLINT NOT NULL,
-      periods_on_list SMALLINT NOT NULL, 
-      pub_date DATE NOT NULL,
-      pub_date_year SMALLINT NULL,
-      pub_date_month SMALLINT NULL,
-      retrieval_date DATE NOT NULL,
-      PRIMARY KEY (list_id, book_rank, pub_date)
-    );
-  END
-GO
-
 -- List Names Table
 IF OBJECT_ID(N'dbo.list_names', N'U') IS NULL
   BEGIN
     CREATE TABLE dbo.list_names (
-      list_id SMALLINT NOT NULL,
+      list_id SMALLINT,
       list_name VARCHAR(100) NOT NULL,
       PRIMARY KEY (list_id)
     );
@@ -61,40 +25,51 @@ IF OBJECT_ID(N'dbo.books', N'U') IS NULL
     );
   END
 GO
-
--- * Constraints *
--- Weeklies to List Names
-BEGIN 
-  ALTER TABLE dbo.list_names
-  ADD CONSTRAINT FK_weeklies_lists
-  FOREIGN KEY (list_id)
-  REFERENCES dbo.weekly_lists (list_id);
-END
+    
+-- Weekly Lists Table
+IF OBJECT_ID(N'dbo.weekly_lists', N'U') IS NULL
+  BEGIN
+    CREATE TABLE dbo.weekly_lists (
+      list_id SMALLINT NOT NULL,
+      book_rank SMALLINT NOT NULL,
+      isbn13 VARCHAR(20) NOT NULL,	
+      rank_last_period SMALLINT NOT NULL,
+      periods_on_list SMALLINT NOT NULL, 
+      pub_date DATE NOT NULL,
+      pub_date_year SMALLINT NULL,
+      pub_date_month SMALLINT NULL,
+      retrieval_date DATE NOT NULL,
+      PRIMARY KEY (list_id, book_rank, pub_date),
+      CONSTRAINT FK_weeklies_listnames
+        FOREIGN KEY (list_id)
+        REFERENCES dbo.list_names(list_id),
+      CONSTRAINT FK_weeklies_books
+        FOREIGN KEY (isbn13)
+        REFERENCES dbo.books(isbn13)
+    );
+  END
 GO
 
--- Weeklies to Books
-BEGIN 
-  ALTER TABLE dbo.books
-  ADD CONSTRAINT FK_weeklies_books
-  FOREIGN KEY (isbn13)
-  REFERENCES dbo.weekly_lists (isbn13);
-END
-GO
-
--- Monthlies to List Names
-BEGIN 
-  ALTER TABLE dbo.list_names
-  ADD CONSTRAINT FK_monthlies_lists
-  FOREIGN KEY (list_id)
-  REFERENCES dbo.monthly_lists (list_id);
-END
-GO
-
--- Monthlies to Books
-BEGIN 
-  ALTER TABLE dbo.books
-  ADD CONSTRAINT FK_monthlies_books
-  FOREIGN KEY (isbn13)
-  REFERENCES dbo.monthly_lists (isbn13);
-END
+-- Monthly Lists Table
+IF OBJECT_ID(N'dbo.monthly_lists', N'U') IS NULL
+  BEGIN
+    CREATE TABLE dbo.monthly_lists (
+      list_id SMALLINT NOT NULL,
+      book_rank SMALLINT NOT NULL,
+      isbn13 VARCHAR(20) NOT NULL,	
+      rank_last_period SMALLINT NOT NULL,
+      periods_on_list SMALLINT NOT NULL, 
+      pub_date DATE NOT NULL,
+      pub_date_year SMALLINT NULL,
+      pub_date_month SMALLINT NULL,
+      retrieval_date DATE NOT NULL,
+      PRIMARY KEY (list_id, book_rank, pub_date),
+      CONSTRAINT FK_monthlies_listnames
+        FOREIGN KEY (list_id)
+        REFERENCES dbo.list_names(list_id),
+      CONSTRAINT FK_monthlies_books
+        FOREIGN KEY (isbn13)
+        REFERENCES dbo.books(isbn13)
+    );
+  END
 GO
