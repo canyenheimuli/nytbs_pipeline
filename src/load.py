7dfb55a5f62a071e8e61a996f17ad0ea8b499002
Canyen Heimuli <46830067+canyenheimuli@users.noreply.github.com>
2026-03-28 22:20:12 -0700

    Update load.py

--- src/load.py
+++ src/load.py
@@ -3,8 +3,10 @@ import os
 from dotenv import load_dotenv
 import pyodbc
 from sqlalchemy import create_engine, text
+from sqlalchemy.engine import Engine
+from urllib.parse import quote_plus
 
-# Get connection engine fn.
+# Get conn string fn.
 def get_azuresqldb_engine() -> Engine:
   '''
   Returns the formatted conn string for
@@ -23,17 +25,17 @@ def get_azuresqldb_engine() -> Engine:
   # driver = '{{ODBC Driver 18 for SQL Server}}' # UPDATE AS NEEDED
 
   # Create string and connect using engine
-  conn_string = f'Driver={driver};\
-                Server=tcp:{server},1433;\
-                Database={database};\
-                Uid={username};\
-                Pwd={password};\
-                Encrypt=yes;\
-                TrustServerCertificate=no;\
-                Connection Timeout=120;'
+  conn_string = f"Driver={driver}; \
+                Server=tcp:{server},1433; \
+                Database={database}; \
+                Uid={username}; \
+                Pwd={password}; \
+                Encrypt=yes; \
+                TrustServerCertificate=no; \
+                Connection Timeout=200;"
 
   conn_url = f"mssql+pyodbc:///?odbc_connect={quote_plus(conn_string)}"
-  
+
   return create_engine(conn_url)
 
 # Conditional table loading function
@@ -41,17 +43,80 @@ def insert_tables(data: dict, engine: Engine) -> None:
   '''
   Perform conditional loading of
   tables based on table 
+  - Lists: Staged inseration only for new records
+  - Books: Staged insertion only for new records
   - Weekly: Normal insertion
   - Monthly: Staged insertion only where staged data is different from most recent data
-  - Books: Staged insertion only for new records
-  - Lists: Staged inseration only for new records
   '''
+  # List info  
+  with engine.connect() as engine_conn:
+      
+      # Set queries
+      stage_table_query = "CREATE TABLE list_info_stage (list_id SMALLINT, list_name VARCHAR(100) NOT NULL);"
+      
+      cond_merge_query = """
+      MERGE INTO list_info AS li
+      USING list_info_stage AS lis
+          ON (li.list_id = lis.list_id)
+      WHEN NOT MATCHED BY TARGET THEN
+          INSERT (list_id, list_name)
+          VALUES (lis.list_id, lis.list_name);
+      """
+      
+      del_stage_query = "DROP TABLE list_info_stage;"
+      
+      # Create stage, load, delete stage, close conn
+      engine_conn.execute(text(stage_table_query))
+      data['list_info'].to_sql('list_info_stage', con = engine_conn, if_exists = 'append', index = False)
+      engine_conn.execute(text(cond_merge_query))
+      engine_conn.execute(text(del_stage_query))
+      engine_conn.commit()
+      engine_conn.close()
+
+  print("List info data loaded to db")
+
+  # Book info
+  with engine.connect() as engine_conn:
+      
+      # Set queries
+      stage_table_query = """
+      CREATE TABLE books_stage (
+          isbn13 VARCHAR(20) NOT NULL,	
+          title VARCHAR(100) NOT NULL,	
+          author VARCHAR(100) NOT NULL, 
+          book_descr VARCHAR(MAX) NULL, 	
+          publisher VARCHAR(100) NULL,
+          book_image VARCHAR(MAX) NULL
+      );
+      """
+      
+      cond_merge_query = """
+      MERGE INTO books AS b
+      USING books_stage AS bs
+          ON (b.isbn13 = bs.isbn13)
+      WHEN NOT MATCHED BY TARGET THEN
+          INSERT (isbn13, title, author, book_descr, publisher, book_image)
+          VALUES (bs.isbn13, bs.title, bs.author, bs.book_descr, bs.publisher, bs.book_image);
+      """
+      
+      del_stage_query = "DROP TABLE books_stage;"
+      
+      # Create stage, load, delete stage, close conn
+      engine_conn.execute(text(stage_table_query))
+      data['books'].to_sql('books_stage', con = engine_conn, if_exists = 'append', index = False)
+      engine_conn.execute(text(cond_merge_query))
+      engine_conn.execute(text(del_stage_query))
+      engine_conn.commit()
+      engine_conn.close()
+  
+  print("Books data loaded to db")
+  
   # Weekly lists
   data['weekly_lists'].to_sql('weekly_lists', con = engine, if_exists = 'append', index = False)
 
+  print("Weekly list data loaded to db")
+
   # Monthly lists
-  # List info
-  # Book info
 
 # Consolidated loader fn
 def load(data: dict) -> None:
@@ -61,13 +126,15 @@ def load(data: dict) -> None:
   where appropriate)
   '''
   # Create connection
+  print("Connecting to database...")
   engine = get_azuresqldb_engine()
+  print("Successfully connected; loading tables...")
 
   # Load all data
   insert_tables(data, engine)
   
   # Status Update
-  print('Tables loaded successfully to database!'
+  print('All tables loaded successfully to database')
 
 # Conditional Execution
 if __name__ == "__main__":
