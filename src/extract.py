69972603f196d2c5625db4e8d28147359e6c5cbd
Canyen Heimuli <46830067+canyenheimuli@users.noreply.github.com>
2026-04-08 00:31:14 -0700

    Update extract.py

--- src/extract.py
+++ src/extract.py
@@ -18,62 +18,67 @@ if not NYT_KEY:
     raise EnvironmentError("NYT API Key is not set. Check your .env file.")
 
 # Get results overview fn
-def get_nytbs_overview(url: str, params: dict | None = None) -> dict:
-  '''
-  Get the rankings from all lists for a given period
-  and return as a python dictionary
-  '''
-  # Params fallback
-  if params is None:
-    params = {"api-key": NYT_KEY}
-  
-  # Try to get overview with exceptions
-  try:
-    # Get response
-    response = requests.get(url = url, params = params, timeout = 10)
-
-    # Raise an exception for bad status codes (4xx or 5xx)
-    response.raise_for_status()
-
-    # Parse the JSON response into a Python dictionary, return
-    return(response.json())
-
-  # HTTP except
-  except requests.exceptions.HTTPError as e:
-    print(f"HTTP error occurred: {e}")
-
-  # API Call except
-  except requests.exceptions.RequestException as e:
-    print(f"An error occurred during the API call: {e}")
-
-  # Key except
-  except json.JSONDecodeError as e:
-      raise RuntimeError(f"Failed to parse JSON response: {e}") from e
+def get_nytbs_overview(url: str, params: dict | None = None, date: str | None = None) -> dict:
+    '''
+    Get the rankings from all lists for a given period
+    and return as a python dictionary
+    '''
+    # Params fallback
+    if params is None:
+        # Set API Key
+        params = {"api-key": NYT_KEY}
+        
+        # Conditionally set date
+        if date is not None:
+            params["published_date"] = date
+    
+    # Try to get overview with exceptions
+    try:
+        # Get response
+        response = requests.get(url = url, params = params, timeout = 10)
+        
+        # Raise an exception for bad status codes (4xx or 5xx)
+        response.raise_for_status()
+        
+        # Parse the JSON response into a Python dictionary, return
+        return(response.json())
+    
+    # HTTP except
+    except requests.exceptions.HTTPError as e:
+        print(f"HTTP error occurred: {e}")
+    
+    # API Call except
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred during the API call: {e}")
+    
+    # Key except
+    except json.JSONDecodeError as e:
+        raise RuntimeError(f"Failed to parse JSON response: {e}") from e
 
 # Clean List Dict fn
 def process_list(book_list, overview) -> pd.DataFrame:
-  '''
-  Turns the book list dict into an individual
-  pd DataFrame including some overview metadata
-  '''
-  return pd.DataFrame(book_list['books']).assign(
-    list_name = book_list['list_name'],
-    list_id = book_list['list_id'],
-    update_freq = book_list['updated'],
-    updated_date = overview['results']['bestsellers_date'],
-    retrieval_date = date.today()
-  )
+    '''
+    Turns the book list dict into an individual
+    pd DataFrame including some overview metadata
+    '''
+    return pd.DataFrame(book_list['books']).assign(
+        list_name = book_list['list_name'],
+        list_id = book_list['list_id'],
+        update_freq = book_list['updated'],
+        pub_date = overview['results']['published_date'],
+        retrieval_date = date.today()
+    )
 
 # Full extract fn
-def extract(url: str = NYT_BSLISTS_OVERVIEW_URL) -> pd.DataFrame:
-
-  # Get overview
-  overview = get_nytbs_overview(url = url)
-
-  # Process and combine results
-  dfs_list = [process_list(book_list, overview) for book_list in overview['results']['lists']]
-  return pd.concat(dfs_list, ignore_index = True)
+def extract(url: str = NYT_BSLISTS_OVERVIEW_URL, date: str | None = None) -> pd.DataFrame:
+    
+    # Get overview
+    overview = get_nytbs_overview(url = url, date = date)
+    
+    # Process and combine results
+    dfs_list = [process_list(book_list, overview) for book_list in overview['results']['lists']]
+    return pd.concat(dfs_list, ignore_index = True)
 
 # Conditional for module importing
 if __name__ == "__main__":
-  extract()
+    extract()
