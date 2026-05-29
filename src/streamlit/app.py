75361e4711839ffb1f681b0dcdf167255b14a799
Canyen Heimuli <46830067+canyenheimuli@users.noreply.github.com>
2026-05-28 22:03:33 -0700

    Added db queries via viz_db.py

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -1,29 +1,51 @@
+# Packages
 import streamlit as st
+import viz_db as vdb
 
+# Back-end (data queries)
+weekly_lists = vdb.query_latest_weeklies(list_id=704) # TO-DO: Update with dynamic list ID retrieval
+monthly_lists = vdb.query_latest_monthlies(list_id=532) # TO-DO: Update with dynamic list ID retrieval
+
+# Landing Page UI
 st.set_page_config(page_title = "NYT BS Lists Dashboard", layout = "wide")
-st.title("Combined Fiction")
+st.title("NYT Bestseller Lists Historical Viewer")
+st.markdown("Use the tabs below to navigate through different pages.")
 
-imgs = [
-    "https://static01.nyt.com/bestsellers/images/9781668236512.jpg", # Theo of Golden
-    "https://static01.nyt.com/bestsellers/images/9780593135228.jpg", # PHM
-    "https://static01.nyt.com/bestsellers/images/9781728289779.jpg", # King of Gluttony???
-    "https://static01.nyt.com/bestsellers/images/9780593804216.jpg", # Yesteryear
-    "https://static01.nyt.com/bestsellers/images/9780593798430.jpg" # The Correspondent
-]
+## Set Tabs
+tab_1, tab_2, tab_3 = st.tabs(["Overview", "Weekly Bestseller Lists", "Monthly Bestsller Lists"])
 
-titles = [
-    "Theo of Golden",
-    "Project Hail Mary",
-    "King of Gluttony",
-    "Yesteryear",
-    "The Correspondent"
-]
+# Overview Tab
+with tab_1:
+    st.title("Overview")
+    st.write("This dashboard shows the current and historical NYT Bestseller Lists.")
+    st.write("Weekly lists include \"Combined Fiction\", and monthly lists include \"Business and Self-help\".")
 
-st.markdown('<meta http-equiv="refresh" content="30">', unsafe_allow_html=True)
+# Weeklies Tab
+with tab_2:
+    # Tab Title
+    st.title("Weekly Lists")
+    
+    # Combined List Section
+    st.header("Combined Print/E-Book Fiction")
+    st_cols = st.columns(len(weekly_lists))
 
-cols = st.columns(len(imgs))
+    # Loop parallel over inputs into st_cols
+    for r_rank, r_title, r_author, r_image, st_col in zip(weekly_lists['rank'], weekly_lists['title'], weekly_lists['author'], weekly_lists['image'], st_cols):
+        with st_col:
+            st.image(r_image, width=100)
+            st.markdown(f"{r_title} by {r_author} ({r_rank})")
 
-for img, title, rank, col in zip(imgs, titles, range(1, 6), cols):
-    with col:
-        st.image(img, width = 100)
-        st.markdown(f"{title} ({rank})")
+# Monthlies Tab
+with tab_3:
+    # Tab Title
+    st.title("Monthly Lists")
+    
+    # Business Section
+    st.header("Business")
+    st_cols = st.columns(len(monthly_lists))
+
+    # Loop parallel over inputs into st_cols
+    for r_rank, r_title, r_author, r_image, st_col in zip(monthly_lists['rank'], monthly_lists['title'], monthly_lists['author'], monthly_lists['image'], st_cols):
+        with st_col:
+            st.image(r_image, width=100)
+            st.markdown(f"{r_title} by {r_author} ({r_rank})")
