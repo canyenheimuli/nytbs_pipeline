5926ffc06a4971ca6715d6b9f289bd15f9eaf4a8
Canyen Heimuli <46830067+canyenheimuli@users.noreply.github.com>
2026-06-05 22:50:34 -0700

    Typo fix in st.col st.markdown call

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -90,7 +90,7 @@ with tab_2:
                         disp_author = r_author if r_author is not None else "Unknown Author"
 
                         # Book info
-                        st.markdown(f"\\#{display_rank}: {display_title} by {display_author}")
+                        st.markdown(f"\\#{disp_rank}: {disp_title} by {disp_author}")
                         
 # Monthlies Tab
 with tab_3:
@@ -132,4 +132,4 @@ with tab_3:
                         disp_author = r_author if r_author is not None else "Unknown Author"
 
                         # Book info
-                        st.markdown(f"\\#{display_rank}: {display_title} by {display_author}")
+                        st.markdown(f"\\#{disp_rank}: {disp_title} by {disp_author}")
