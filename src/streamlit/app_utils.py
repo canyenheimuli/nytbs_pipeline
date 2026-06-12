07a0058b3f37cb768c361d7fd50a97ff6b0ec2b1
Canyen Heimuli <46830067+canyenheimuli@users.noreply.github.com>
2026-06-11 23:00:30 -0700

    Potential bug fix in process_list_df() by making copy

--- src/streamlit/app_utils.py
+++ src/streamlit/app_utils.py
@@ -7,6 +7,9 @@ def process_list_df(lists_df: pd.DataFrame, lists_freq: str) -> list[pd.DataFram
     Re-order and split a weekly or monthly 
     full DF of period-specific lists
     '''
+    # Make copy of input
+    output_df = lists_df.copy()
+    
     # Set List ID Order
     if lists_freq == "weekly":
         list_ids_order = [704, 708, 1, 2, 17, 24, 10, 14, 13, 7] # Lists are ordered subjectively based on list popularity and intended age ranges
@@ -14,11 +17,11 @@ def process_list_df(lists_df: pd.DataFrame, lists_freq: str) -> list[pd.DataFram
         list_ids_order = [10018, 301, 302, 10004, 532, 304, 719, 10016, 10015, 303] # Lists are ordered subjectively based on list popularity and intended age ranges
 
     # Make new var, arrange
-    lists_df['list_id_sorted'] = pd.Categorical(
-        lists_df['list_id'], 
+    output_df['list_id_sorted'] = pd.Categorical(
+        output_df['list_id'], 
         categories=list_ids_order, 
         ordered=True
     )
-    lists_df = lists_df.sort_values(by=['list_id_sorted', 'rank'])
+    output_df = output_df.sort_values(by=['list_id_sorted', 'rank'])
     
-    return [group for _, group in lists_df.groupby('list_id_sorted', observed=True)]
+    return [group for _, group in output_df.groupby('list_id_sorted', observed=True)]
