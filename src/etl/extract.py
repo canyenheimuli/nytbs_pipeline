a05883deb435c44516b10311c9b15c5db9c3268b
Canyen Heimuli <46830067+canyenheimuli@users.noreply.github.com>
2026-06-18 00:15:19 -0700

    Added retry cf for error code 500

--- src/etl/extract.py
+++ src/etl/extract.py
@@ -1,6 +1,7 @@
 # Packages
 import requests
 import json
+import time
 import pandas as pd
 from datetime import date
 import os
@@ -18,7 +19,7 @@ if not NYT_API_KEY:
     raise EnvironmentError("NYT API Key is not set. Check your .env file.")
 
 # Get results overview fn
-def get_nytbs_overview(url: str, params: dict | None = None, date: str | None = None) -> dict:
+def get_nytbs_overview(url: str, params: dict | None = None, date: str | None = None, err_500_attempts: int = 3, err_500_retry_wait: int = 60) -> dict:
     '''
     Get the rankings from all lists for a given period
     and return as a python dictionary
@@ -33,27 +34,40 @@ def get_nytbs_overview(url: str, params: dict | None = None, date: str | None = 
             params["published_date"] = date
     
     # Try to get overview with exceptions
-    try:
-        # Get response
-        response = requests.get(url = url, params = params, timeout = 10)
+    while err_500_attempts > 0:
+        # Attempt request
+        try:
+            # Get response
+            response = requests.get(url = url, params = params, timeout = 10)
+            
+            # Raise an exception for bad status codes (4xx or 5xx)
+            response.raise_for_status()
+            
+            # Parse the JSON response into a Python dictionary, return
+            return(response.json())
         
-        # Raise an exception for bad status codes (4xx or 5xx)
-        response.raise_for_status()
+        # HTTP except
+        except requests.exceptions.HTTPError as e:
+            if e.response.status_code == 500:
+                # Status Code 500 control flow
+                err_500_attempts -= 1
+                print(f"Received Status Code 500; retrying in {err_500_retry_wait} seconds")
+                time.sleep(err_500_retry_wait)
+                continue
+            else:
+                # Other
+                print(f"HTTP error occurred: {e}")
+                break
         
-        # Parse the JSON response into a Python dictionary, return
-        return(response.json())
-    
-    # HTTP except
-    except requests.exceptions.HTTPError as e:
-        print(f"HTTP error occurred: {e}")
-    
-    # API Call except
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred during the API call: {e}")
-    
-    # Key except
-    except json.JSONDecodeError as e:
-        raise RuntimeError(f"Failed to parse JSON response: {e}") from e
+        # API Call except
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred during the API call: {e}")
+            break
+        
+        # Key except
+        except json.JSONDecodeError as e:
+            raise RuntimeError(f"Failed to parse JSON response: {e}") from e
+            break
 
 # Clean List Dict fn
 def process_list(book_list: dict, overview: dict) -> pd.DataFrame:
@@ -71,7 +85,10 @@ def process_list(book_list: dict, overview: dict) -> pd.DataFrame:
 
 # Full extract fn
 def extract(url: str = NYT_BSLISTS_OVERVIEW_URL, date: str | None = None) -> pd.DataFrame:
-    
+    '''
+    Gets the overview JSON from the API and
+    processes the list into a pandas dataframe
+    '''
     # Get overview
     overview = get_nytbs_overview(url = url, date = date)
     
