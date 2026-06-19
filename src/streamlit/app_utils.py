2be757fdb06753282cd13c982e1b670e42e730d9
Canyen Heimuli <46830067+canyenheimuli@users.noreply.github.com>
2026-06-18 22:49:28 -0700

    Added fun to get cycle code (most recent Friday)

--- src/streamlit/app_utils.py
+++ src/streamlit/app_utils.py
@@ -1,5 +1,5 @@
 # Packages
-from datetime import date
+from datetime import datetime, date, timedelta
 import pandas as pd
 
 # Benchmark user-supplied date to nearest DB week
@@ -10,6 +10,18 @@ def find_nearest_week(selected_date: date, available_weeks: list[date]) -> date:
     '''
     past_weeks = [w for w in available_weeks if w <= selected_date]
     return max(past_weeks) if past_weeks else None
+
+# Get code for current cycle (week)
+def get_cycle_code():
+    '''
+    Gets string for the current (or last)
+    week's Friday to use in cache cycle
+    '''
+    # Get date, benchmark to most recent Friday, and use as a cache key
+    today = datetime.now().date()
+    days_since_fri = (today.weekday() - 4) % 7
+    most_recent_fri = today - timedelta(days=days_since_fri)
+    return most_recent_fri.strftime("%Y-%m-%d")
 
 # Re-order and Split Queried List Data fn.
 def process_list_df(lists_df: pd.DataFrame, lists_freq: str) -> list[pd.DataFrame]:
